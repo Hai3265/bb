c74e0053e16a621b819d906f06d9c1517c912cf3
Hai3265 <92850315+Hai3265@users.noreply.github.com>
2021-11-24 11:44:38 +0800

    FDS
    DFD

--- src/main/java/com/yr/test/A.java
+++ src/main/java/com/yr/test/A.java
@@ -5,8 +5,7 @@ public class A {
 	public static void main(String[] args) {
 		System.out.println("aaa");
 		System.out.println("aaa");
-		System.out.println("CC");
-		System.out.println("CCC");
+		
 	}
 
 }
